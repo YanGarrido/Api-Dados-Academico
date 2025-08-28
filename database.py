ecc0990d80bd23abe9ae8b96bac40c43ff035688
Yan <yangarridodev@gmail.com>
2025-08-28 09:44:10 -0300

    criação do banco de dados

---
+++ database.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT") 
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_DRIVER = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
+
+encoded_password = quote_plus(DB_PASSWORD)
+
+SQLALCHEMY_DATABASE_URL = (
+    f"mssql+pyodbc://{DB_USERNAME}:{encoded_password}@{DB_HOST},{DB_PORT}/{DB_DATABASE}?"
+    f"driver={DB_DRIVER}&TrustServerCertificate=yes")
+
+engine =create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+  db= SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
