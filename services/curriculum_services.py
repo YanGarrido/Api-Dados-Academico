a5924e0a8b58a7d2d773e6c33ad4ab91107d5b1f
Yan <yangarridodev@gmail.com>
2025-09-04 09:04:58 -0300

    requisição para trazer todos os currículos

--- services/curriculum_services.py
+++ services/curriculum_services.py
@@ -2,7 +2,29 @@ from sqlalchemy.orm import Session
 from sqlalchemy import text
 from schemas.curriculum_schema import CurriculumInfo
 
-async def get_curriculum_current(curso: str,db:Session):
+
+async def get_curriculum(db:Session):
+  """
+  Executa a query SQL para buscar o currículo de um curso específico.
+  """
+  try:
+    sql_query = text("""
+    SELECT 
+    s.CODPERIODO AS periodo, 
+    s.CODCURSO AS curso_id, 
+    s.CODDISC AS disciplina_id,
+    s.CODGRADE AS codgrade,
+    s.DESCRICAO AS descricao  
+    FROM CEMGJB_128187_RM_DV.dbo.SDISCGRADE s 
+    WHERE s.CODGRADE ='20231' AND s.CODPERIODO != 0
+    """)
+    results = db.execute(sql_query).mappings().all()
+    return results
+  except Exception as e:
+    print(f"Erro ao executar a consulta SQL: {e}")
+    raise e
+
+async def get_curriculum_current_course(curso: str,db:Session):
   """
   Executa a query SQL para buscar o currículo de um curso específico.
   """
